04bfa7bb229b77849499166f75a2305097cdfce5
SwarnaIslam <bsse1214@iit.du.ac.bd>
2023-12-20 02:08:00 +0600

    local library

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 const dev_db_url =
   "mongodb+srv://your_user_name:your_password@cluster0.lz91hw2.mongodb.net/local_library?retryWrites=true&w=majority";
-const mongoDB = process.env.MONGODB_URI || dev_db_url;
+const mongoDB = process.env.MONGO_PRIVATE_URL || dev_db_url;
 async function main() {
   await mongoose.connect(mongoDB);
 }
